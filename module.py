051c5dae0ca02ae7f317b0c7acd3f23ae4be0903
ioan beilic <ioan@ioans-MacBook-Pro.local>
2026-02-24 00:24:10 +0100

    fix: remove leading indentation from module.py

--- module.py
+++ module.py
@@ -1,30 +1,30 @@
-    from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
-    MODULE_ID = 'ecommerce'
-    MODULE_NAME = _('Online Store')
-    MODULE_VERSION = '1.0.0'
-    MODULE_ICON = 'globe-outline'
-    MODULE_DESCRIPTION = _('Online store integrated with inventory and payments')
-    MODULE_AUTHOR = 'ERPlora'
-    MODULE_CATEGORY = 'commerce'
+MODULE_ID = 'ecommerce'
+MODULE_NAME = _('Online Store')
+MODULE_VERSION = '1.0.0'
+MODULE_ICON = 'globe-outline'
+MODULE_DESCRIPTION = _('Online store integrated with inventory and payments')
+MODULE_AUTHOR = 'ERPlora'
+MODULE_CATEGORY = 'commerce'
 
-    MENU = {
-        'label': _('Online Store'),
-        'icon': 'globe-outline',
-        'order': 19,
-    }
+MENU = {
+    'label': _('Online Store'),
+    'icon': 'globe-outline',
+    'order': 19,
+}
 
-    NAVIGATION = [
-        {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
+NAVIGATION = [
+    {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
 {'label': _('Products'), 'icon': 'storefront-outline', 'id': 'products'},
 {'label': _('Orders'), 'icon': 'cart-outline', 'id': 'orders'},
 {'label': _('Settings'), 'icon': 'settings-outline', 'id': 'settings'},
-    ]
+]
 
-    DEPENDENCIES = []
+DEPENDENCIES = []
 
-    PERMISSIONS = [
-        'ecommerce.view_onlineorder',
+PERMISSIONS = [
+    'ecommerce.view_onlineorder',
 'ecommerce.change_onlineorder',
 'ecommerce.manage_settings',
-    ]
+]
